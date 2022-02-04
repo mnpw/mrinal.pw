deb6522ef98eddca48df6411458bc74fb94dd7bb
Mrinal Paliwal <mrinal16164@iiitd.ac.in>
2022-02-04 16:59:37 +0530

    Add new logo, add transition

--- components/layout.js
+++ components/layout.js
@@ -3,12 +3,18 @@ import styles from './layout.module.css'
 import Link from 'next/link'
 import utilStyles from '../styles/utils.module.css'
 import Image from 'next/image'
-import logo from '../public/website_logo.svg'
+import logo_2 from '../public/website_logo.svg'
+import logo_1 from '../public/dp.png'
+import { useState } from 'react'
 
 const name = 'Mrinal Paliwal'
 export const siteTitle = 'mnpw\'s garden'
 
 export default function Layout({ children, home }) {
+
+    const logo1 = { src: logo_1, width: 34, height: 34 }
+    const logo2 = { src: logo_2, width: 80, height: 34 }
+
     return (
         <div className={styles.container}>
             <Head>
@@ -21,7 +27,15 @@ export default function Layout({ children, home }) {
             </Head>
             <header className={styles.header}>
                 <Link href="/">
-                    <Image className={styles.logo} src={logo} height={26} width={85} />
+                    {/* onMouseOver={() => changeLogo(logo2)} onMouseOut={() => changeLogo(logo1)} */}
+                    <div className={styles.logo}>
+                        <span className={styles.logo_hover}>
+                            <Image src={logo2.src} height={logo2.height} width={logo2.width} />
+                        </span>
+                        <span className={styles.logo_base}>
+                            <Image className={styles.logo_base} src={logo1.src} height={logo1.height} width={logo1.width} />
+                        </span>
+                    </div>
                 </Link>
                 <span>
                     <Link href="/now/" ><a className={styles.navbar}>Now</a></Link>
