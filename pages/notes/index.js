9a06d354086cb7780c78bf44c09e417dcd89f4f9
Mrinal Paliwal <mrinal16164@iiitd.ac.in>
2022-05-07 00:35:40 +0530

    Add date tag in notes

--- pages/notes/index.js
+++ pages/notes/index.js
@@ -6,18 +6,21 @@ export default function Posts() {
     return (
         <Layout>
             <h2 className={utilStyles.headingXl}>Notes</h2>
+            <div style={{ marginTop: "2rem", marginBottom: "1.2rem" }}>
+                <h3 className={utilStyles.headingMd}>2022</h3>
+            </div>
             <ul className={utilStyles.list}>
                 <li className={utilStyles.listItem}>
-                    <Link href="/pushing-against-reality/" ><a>Pushing against reality</a></Link>
+                    <span className={utilStyles.labelText}>03 mar</span> <Link href="/pushing-against-reality/" ><a>Pushing against reality</a></Link>
                 </li>
                 <li className={utilStyles.listItem}>
-                    <Link href="/words-are-sacred/" ><a>Words are sacred</a></Link>
+                    <span className={utilStyles.labelText}>15 feb</span> <Link href="/words-are-sacred/" ><a>Words are sacred</a></Link>
                 </li>
                 <li className={utilStyles.listItem}>
-                    <Link href="/poking-the-system/" ><a>Poking the system</a></Link>
+                    <span className={utilStyles.labelText}>10 feb</span> <Link href="/poking-the-system/" ><a>Poking the system</a></Link>
                 </li>
                 <li className={utilStyles.listItem}>
-                    <Link href="/mothers-arguments-internet/" ><a>Mothers, arguments, and the internet</a></Link>
+                    <span className={utilStyles.labelText}>31 jan</span> <Link href="/mothers-arguments-internet/" ><a>Mothers, arguments, and the internet</a></Link>
                 </li>
             </ul>
         </Layout>
