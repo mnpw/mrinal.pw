d8e253fc36de4a56db6e7ce6b096015d85c7c251
Mrinal Paliwal <mrinal16164@iiitd.ac.in>
2022-05-30 19:54:34 +0530

    Add rust code to parse `.md` files

---
+++ rust/src/lib.rs
@@ -0,0 +1,31 @@
+mod utils;
+
+use pulldown_cmark::{html, Options, Parser};
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}!", name));
+}
+
+#[wasm_bindgen]
+pub fn parse_markdown_to_html(markdown: &str) -> String {
+    let mut options = Options::empty();
+    options.insert(Options::ENABLE_STRIKETHROUGH);
+    let parser = Parser::new_ext(markdown, options);
+
+    let mut html_output: String = String::with_capacity(markdown.len() * 3 / 2);
+    html::push_html(&mut html_output, parser);
+    html_output
+}
