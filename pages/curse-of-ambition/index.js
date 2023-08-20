019677a98dcd9e63fe6e8f8de3d597a4749686cc
Mrinal Paliwal <mrinal16164@iiitd.ac.in>
2023-08-20 20:14:21 +0530

    edits

--- pages/curse-of-ambition/index.js
+++ pages/curse-of-ambition/index.js
@@ -12,16 +12,16 @@ export default function Posts() {
             <p>Something similar happens in interval running (It is when you walk and run in intervals. Say, repeating a minute of walking and five of running). There are many variations of it but in all it is much easier to slip from the running interval to the walking interval. Moving from the walking state to the running state is always harder.</p>
             <p>This moment of resistance is not created equal. You can drop and pick up a pebble as many times as you want. But you can pick up a dropped boulder again only so many times. It is easier to pick up a pebble than a boulder.</p>
             <p>~</p>
-            <p>When I am programming I think about the second and the third order effects. How failures cascade and how systems change with the tiniest of perturbations. The act of programming requires a bigger working memory from me, a bigger <em>stack</em>.</p>
-            <p>Much of focussing in programming feels like lifting a heavy rock. And the heavier the rock, the harder any interruptions around me hit. The cost of dropping a heavy rock is high. It takes the fastest to drop the heaviest rock and takes the longest to pick it back up.</p>
+            <p>When I am programming I think about the second and the third order effects. How failures cascade and how systems change with the tiniest of perturbations. The act of programming requires a bigger working memory from me — a bigger <em>stack</em>.</p>
+            <p>Much of focussing during programming feels like lifting a heavy rock. And the heavier the rock, the harder any interruptions hit. The cost of dropping a heavy rock is high. It takes the fastest to drop the heaviest rock and takes the longest to pick it back up.</p>
             <p>When I program, I do so with the curse of ambition. And when the wind of <em>trends</em> and <em>errands</em> push against me I find my ambition disoriented.</p>
             <p>There's a new big-thing and a new errand-to-run everywhere I look today. They shake my ambition and test my resolve. They make it hard to stick with my craft and my ambition. I find my ambition withering and warping around the winds of interruption. Sometimes when this wind flicks the heavy rock away from my shoulders, I start looking for smaller ones to carry. It is easier to pick up a pebble than a boulder after all.</p>
             <p>Interruptions can run your ambition dry.</p>
             <p>~</p>
             <p>All important things take time, they say, and I believe that. All important things are important because they require a copious amount of the most valuable currency there is — one's time and energy.</p>
-            <p>Importance is alchemy over time, it is the emergent property of concentrated energy.</p>
+            <p>Importance is alchemy applied on time, it is the emergent property of concentrated energy.</p>
             <p>Important becomes remarkable with the binding agent of ambition. Ambition guides time and energy to something remarkable. But when there are strong currents testing your ambition all around, it is not easy to keep going. The currents sway me in other directions, they tempt me to drop the boulder for the pebble.</p>
-            <p>If I carried a pebble instead of a boulder, would I be happier? I don't know. Perhaps I'll never know, for I am cursed with ambition.</p>
+            <p>If I carried a pebble instead of a boulder, would I be happier? I don't know. Perhaps I'll never know; it is the curse of ambition.</p>
         </Layout>
     )
 }
